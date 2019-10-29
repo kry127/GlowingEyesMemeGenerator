d561efbebee2ad9b1ae390e4a23258c0010cf38c
Kry127 <kry127@yandex.ru>
2019-10-29 13:39:00 +0300

    Added UI

--- posts/forms.py
+++ posts/forms.py
@@ -7,3 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'cover']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'input'}),
+            'cover': forms.FileInput(attrs={'class': 'my_input'})
+        }
